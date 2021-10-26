00ab87aee333960588f5a5f9f11b8e6be467a519
galenczk <galenczk@gmail.com>
2021-10-26 06:06:59 -0700

    Set up the 16 x 16 grid

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+function createPage(number) {
+  for (i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    for (j = 0; j < 16; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
+    }
+  }
+}
+
+createPage();
